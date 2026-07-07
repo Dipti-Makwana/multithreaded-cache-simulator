e31ae4a1ca875d474bced4b29967b4505d9e1bae
Dipti-Makwana <dipti3dipti@gmail.com>
2026-07-07 15:30:36 +0530

    Implement get() method for cache hit/miss check with recency update

--- src/LRUCache.java
+++ src/LRUCache.java
@@ -3,13 +3,15 @@ import java.util.HashMap;
 class LRUCache {
     private int capacity;
     private HashMap<Integer, Node> map;
-    private Node head;
-    private Node tail;
+    private Node head; // dummy head (Most Rrecently Used side)
+    private Node tail; // demmy tail (Least Recently Used side)
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
         this.map = new HashMap<>();
 
+
+    // dummy nodes with fake address -1 (never real data)
         head = new Node(-1);
         tail = new Node(-1);
 
@@ -17,7 +19,8 @@ class LRUCache {
         tail.prev = head;
 
     }
-
+    
+    // removes a node from its current position in the list
     private void remove(Node node) {
         Node prevNode = node.prev;
         Node nextNode = node.next;
@@ -27,6 +30,7 @@ class LRUCache {
 
     }
 
+    // inserts a node right after head (marks it as Most Recently Used)
     private void insertAtFront(Node node) {
         Node firstRealNode = head.next;
 
@@ -36,4 +40,21 @@ class LRUCache {
         node.next = firstRealNode;
         firstRealNode.prev = node; 
     }
-}
+
+    // returns true if address is a HIT, false if it's a MISS
+    public boolean get(int address) {
+        if (map.containsKey(address)) {
+            // HIT - address alreaady in cache
+            Node node = map.get(address);
+
+            // move it ti front since it's now the most recently used
+            remove(node);
+            insertAtFront(node);
+
+            return true;  // hit
+        }
+        return false; // miss
+        }
+    }
+
+
