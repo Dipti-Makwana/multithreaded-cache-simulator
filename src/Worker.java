cb61b30e101fffe9a15991559a3ff0e40c6bd5a1
Dipti-Makwana <dipti3dipti@gmail.com>
2026-07-09 17:19:17 +0530

    Add Worker class implementing Runnable for multi-threaded cache access simulation

---
+++ src/Worker.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+ class Worker implements Runnable {
+    private Cache cache;
+    private int numberOfAccesses;
+    private int addressRange;
+
+    public Worker(Cache cache, int numberOfAccesses, int addressRaange) {
+        this.cache = cache;
+        this.numberOfAccesses = numberOfAccesses;
+        this.addressRange = addressRange;
+        
+    }
+
+    @Override 
+    public void run() {
+        Random random = new Random();
+
+        for (int i = 0; i< numberOfAccesses; i++) {
+            int address = random.nextInt(addressRange);
+            cache.access(address);
+        }
+    }
+
+ }
